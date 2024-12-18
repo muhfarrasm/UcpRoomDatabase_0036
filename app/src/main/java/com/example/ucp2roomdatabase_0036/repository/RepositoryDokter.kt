1cef0589cc422262e7756e7780dcadbeb85f4329
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-18 17:37:19 +0700

    Membuat RepositoryDokter

---
+++ app/src/main/java/com/example/ucp2roomdatabase_0036/repository/RepositoryDokter.kt
@@ -0,0 +1,16 @@
+package com.example.ucp2roomdatabase_0036.repository
+
+import com.example.ucp2roomdatabase_0036.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryDokter {
+
+    // method ini untuk memanggil fungsi geetAllMahasiswa dari mahasiswaDao untuk
+    //mendapatkan semua data dlm bentuk aliran
+    fun getAllDokter() : Flow<List<Dokter>>
+    //method ini untuk memanggil fungsi geetAllMahasiswa dari mahasiswaDao berdasarkan NIM
+    fun getDokterById(Id: String): Flow<Dokter?>
+
+    //Method ini memanfaatkan fungsi insertMahasiswa dari MahasiswaDao
+    suspend fun insertDokter(dokter: Dokter)
+}
