38e0f882bfa36662315514a4b114d405bdc663e1
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-18 17:32:27 +0700

    Membuat Dao Jadwal

--- app/src/main/java/com/example/ucp2roomdatabase_0036/data/dao/JadwalDao.kt
+++ app/src/main/java/com/example/ucp2roomdatabase_0036/data/dao/JadwalDao.kt
@@ -1,4 +1,30 @@
 package com.example.ucp2roomdatabase_0036.data.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2roomdatabase_0036.data.entity.Dokter
+import com.example.ucp2roomdatabase_0036.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface JadwalDao {
+
+    //getAll Jadwal
+    @Query("SELECT * FROM Jadwal ORDER BY id ASC")
+    fun getAllJadwal(): Flow<List<Jadwal>>
+
+    @Query("SELECT * FROM Jadwal WHERE id = :id")
+    fun getJadwalByid(id: String): Flow<Jadwal?>
+
+    @Insert
+    suspend fun insertJadwal(jadwal: Jadwal)
+
+    @Delete
+    suspend fun DeleteJadwal(jadwal: Jadwal)
+
+    @Update
+    suspend fun UpdateJadwal(jadwal: Jadwal)
 }
