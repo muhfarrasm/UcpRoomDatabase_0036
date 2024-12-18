5b810b0e3d7b5797ac629aeb71cc34a4fc189dc4
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-18 17:41:47 +0700

    Membuat RepositoryJadwal

---
+++ app/src/main/java/com/example/ucp2roomdatabase_0036/repository/RepositoryJadwal.kt
@@ -0,0 +1,26 @@
+package com.example.ucp2roomdatabase_0036.repository
+
+import androidx.room.Delete
+import androidx.room.Update
+import com.example.ucp2roomdatabase_0036.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryJadwal {
+
+    // method ini untuk memanggil fungsi geetAllMahasiswa dari mahasiswaDao untuk
+    //mendapatkan semua data dlm bentuk aliran
+    fun getAllJadwal() : Flow<List<Jadwal>>
+    //method ini untuk memanggil fungsi geetAllMahasiswa dari mahasiswaDao berdasarkan NIM
+    fun getJadwalById(id: String): Flow<Jadwal?>
+
+    //Method ini memanfaatkan fungsi insertMahasiswa dari MahasiswaDao
+    suspend fun InsertJadwal(jadwal: Jadwal)
+
+    //Method ini memanfaatkan fungsi deleteMahasiswa dari MahasiswaDao
+    suspend fun DeleteJadwal(jadwal: Jadwal)
+
+    //Method ini memanfaatkan fungsi updateMahasiswa dari MahasiswaDao
+    suspend fun UpdateJadwal(jadwal: Jadwal)
+
+
+}
