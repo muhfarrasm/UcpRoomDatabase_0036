e388513fc4c34aae7650f9268590dbdf6d6da67b
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-20 11:02:24 +0700

    Membuat JadwalApp untuk menggunakan databaseroom sebagai pengelola data dari repositoryjadwal

---
+++ app/src/main/java/com/example/ucp2roomdatabase_0036/dependenciesinjection/JadwalApp.kt
@@ -0,0 +1,18 @@
+package com.example.ucp2roomdatabase_0036.dependenciesinjection
+
+import android.content.Context
+import com.example.ucp2roomdatabase_0036.data.database.RumahSakitDatabase
+
+import com.example.ucp2roomdatabase_0036.repository.LocalRepositoryJadwal
+
+import com.example.ucp2roomdatabase_0036.repository.RepositoryJadwal
+
+interface InterfaceJadwalApp{
+    val repositoryJadwal: RepositoryJadwal
+}
+
+class JadwalApp(private val context: Context) : InterfaceJadwalApp{
+    override val repositoryJadwal: RepositoryJadwal by lazy {
+        LocalRepositoryJadwal(RumahSakitDatabase.getDatabase(context).JadwalDao())
+    }
+}
