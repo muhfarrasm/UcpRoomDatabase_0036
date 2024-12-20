a446b8f6e6aa65f19aef1c4f7bb97f694785215e
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-20 13:20:32 +0700

    Membuat HomeDokterViewModel untuk menarik data dari database/ repository ke view

---
+++ app/src/main/java/com/example/ucp2roomdatabase_0036/ui/viewmodel/dokter/HomeDokterViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.ucp2roomdatabase_0036.ui.viewmodel.dokter
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2roomdatabase_0036.data.entity.Dokter
+import com.example.ucp2roomdatabase_0036.repository.RepositoryDokter
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeDokterViewModel(
+    private val repositoryDokter: RepositoryDokter
+) : ViewModel(){
+    val homeUiState: StateFlow<HomeDokterUiState> = repositoryDokter.getAllDokter()
+        .filterNotNull()
+        .map {
+            HomeDokterUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeDokterUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeDokterUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessages = it.message ?:"Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeDokterUiState(
+                isLoading = true,
+            )
+        )
+
+}
+
+data class HomeDokterUiState(
+    val listMhs: List<Dokter> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessages: String = ""
+)
